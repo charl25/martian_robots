b03f88fdfbdd042eff51bf06e86c0a62440d42b8
charl25 <43599003+charl25@users.noreply.github.com>
2025-07-11 09:10:52 +0200

    Create martian_robots.py
    created the martian_robots file and added dome skeleton code to continue task on

---
+++ martian_robots.py
@@ -0,0 +1,23 @@
+import sys
+
+DIRECTIONS = ['N', 'E', 'S', 'W']
+
+def store_instructions(file_name):
+    pass
+
+
+def move(loc,orientation):
+    pass
+
+
+def turn(current, direction):
+    pass
+
+
+def run_instructions(instructions):
+    pass
+
+
+if __name__ == "__main__":
+
+    pass
